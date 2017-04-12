d92ef1a73603e7c7bf41960649b83d99607d5cff
Samantha <samanthakucher@gmail.com>
2017-04-12 15:32:11 -0300

    Distribuciones para variables aleatorias discretas

---
+++ Guia2/distrib.py
@@ -0,0 +1,39 @@
+import math as m
+
+def combinatoria(n,k):
+	return float(m.factorial(n)/(m.factorial(n-k)*m.factorial(k)))
+
+def binomial(k, n, p):
+	#Variable aleatoria = k (cantidad de exitos)
+	#p = cte
+	return combinatoria(n,k)*(p**k)*((1-p)**(n-k))
+
+def binomial_neg(k, n, p):
+	#Variable aleatoria = n (cantidad de intentos)
+	#El ultimo es un exito, k no es aleatorio
+	return combinatoria(n-1, k-1)*(p**k)*((1-p)**(n-k))
+
+def hipergeometrica(k, t, n, q):
+	#p =! cte
+	#k exitos, t cosas en total, q cosas que quiero, n intentos
+	return (combinatoria(q,k)*combinatoria(t-q,n-k))/combinatoria(t,n)
+
+#GUIA 2
+ej1 = binomial(3,10,1.00/6.00) 
+#OBS: si pongo 1/6 python los interpreta como enteros y me devuelve un entero
+
+ej2 = hipergeometrica(5, 100, 10, 80)
+
+i = 1
+ej6 = 0
+while (i<=25):
+	ej6 = binomial(i,25,1.00/36.00) + ej6
+	i = i+1
+
+j = 3
+ej7 = 0
+while (j<=5):
+	ej7 = hipergeometrica(j,10,5,5) + ej7
+	j = j+1
+
+print(ej7)
